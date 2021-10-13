f23bf62bd6d07db23a5bd2cc34bc932e05790058
liqiang <liqiang05@kanzhun.com>
2021-10-13 15:30:43 +0800

    名称更改

---
+++ TestCC/Pods/Headers/Public/TestPodCC/ViewController.h
@@ -0,0 +1,1 @@
+../../../../../TestPodCC/ViewController.h
---
+++ TestCC/TestCC/LQFunc.m
@@ -0,0 +1,15 @@
+//
+//  LQFunc.m
+//  TestCC
+//
+//  Created by lq on 2021/10/11.
+//
+
+#import "LQFunc.h"
+
+@implementation LQFunc
+
+- (void)outPutTestLog {
+    NSLog(@" Created by lq on 2021/10/11.");
+}
+@end
